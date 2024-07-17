746f3e5c08ea2b0ad24fc402813cf3c36e696155
Atem1972 <barronvalery@gmail.com>
2024-07-17 19:46:18 +0200

    new code

---
+++ ec2.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "server1" {
+    ami = "ami-08293cd37d25b872d"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.sg1.id]
+    availability_zone = "us-east-1a"
+    subnet_id = aws_subnet.private1.id
+    user_data = file("code.sh")
+    tags = {
+      Name = "webserver1"
+    }
+  
+}
+
+resource "aws_instance" "server2" {
+    ami = "ami-08293cd37d25b872d"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.sg1.id]
+    availability_zone = "us-east-1b"
+    subnet_id = aws_subnet.private2.id
+    user_data = file("code.sh")
+    tags = {
+      Name = "webserver2"
+    }
+  
+}
