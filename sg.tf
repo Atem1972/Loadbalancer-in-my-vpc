a2e4c19fa06233154ff0607544fa5ca6d3582070
Atem1972 <barronvalery@gmail.com>
2024-07-17 21:44:23 +0200

    code

--- sg.tf
+++ sg.tf
@@ -1,7 +1,7 @@
 
 
-resource "aws_security_group" "sg1" {
-    name = "terraform-sg"
+resource "aws_security_group" "sg" {
+    name = "Terraform-1"
     vpc_id = aws_vpc.vp1.id
     description = "Allow ssh and httpd"
     
@@ -12,7 +12,7 @@ resource "aws_security_group" "sg1" {
         to_port = 80
         protocol = "tcp"
         #cidr_blocks = ["0.0.0.0/0"]    # this can not be open to the world bc its our ec2 sg
-        security_groups = [aws_security_group.sg2.name]
+        security_groups = [aws_security_group.sg2.id]
     }
     
  
